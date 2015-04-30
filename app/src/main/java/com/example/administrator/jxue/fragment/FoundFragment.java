974beaa5f8f7d8b8e44a2a4052d84f88231bfa1f
luo697 <245220291@qq.com>
2015-04-30 16:46:26 +0800

    修改

--- app/src/main/java/com/example/administrator/jxue/fragment/FoundFragment.java
+++ app/src/main/java/com/example/administrator/jxue/fragment/FoundFragment.java
@@ -11,11 +11,10 @@ import android.view.ViewGroup;
 import com.example.administrator.jxue.R;
 import com.example.administrator.jxue.foundVPadapter;
 
-/**
- * Created by Administrator on 2015-4-30.
- */
+
 public class FoundFragment extends Fragment implements ViewPager.OnPageChangeListener {
     private ViewPager fondviewpager;
+    private View[] views=new View[3];
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -27,7 +26,12 @@ public class FoundFragment extends Fragment implements ViewPager.OnPageChangeLis
     private void init(View view) {
         fondviewpager = ((ViewPager) view.findViewById(R.id.foundViewpager));
         fondviewpager.setAdapter(new foundVPadapter(getFragmentManager()));
+        fondviewpager.setCurrentItem(1);
         fondviewpager.setOnPageChangeListener(this);
+        views[0] = view.findViewById(R.id.foundview1);
+        views[1] = view.findViewById(R.id.foundview2);
+        views[2] = view.findViewById(R.id.foundview3);
+
     }
 
 
@@ -38,7 +42,13 @@ public class FoundFragment extends Fragment implements ViewPager.OnPageChangeLis
 
     @Override
     public void onPageSelected(int position) {
-
+        for(int i=0;i<views.length;i++){
+            if(position==i){
+                views[i].setVisibility(View.VISIBLE);
+            }else{
+                views[i].setVisibility(View.GONE);
+            }
+        }
     }
 
     @Override
